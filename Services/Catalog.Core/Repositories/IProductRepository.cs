4ee4137b16eb2f409ad0f8fb9afbbeb20f02c4a5
masood <5045941-masoodMEH@users.noreply.gitlab.com>
2026-03-24 16:56:30 +0330

    add new interfaces #6

--- Services/Catalog.Core/Repositories/IProductRepository.cs
+++ Services/Catalog.Core/Repositories/IProductRepository.cs
@@ -1,5 +1,19 @@
-﻿namespace Catalog.Core.Repositories;
+﻿using Catalog.Core.Entities;
+
+namespace Catalog.Core.Repositories;
 
 public interface IProductRepository
 {
+    Task<IEnumerable<Product>> GetAllProducts();
+    Task<Product> GetById(string id);
+
+    Task<IEnumerable<Product>> GetProductsByName(string name);
+    Task<IEnumerable<Product>> GetProductsByTypeId(string typeId);
+
+    Task<IEnumerable<Product>> GetProductsByBrand(string brand);
+    Task<IEnumerable<Product>> GetProductsByBrandId(string brandId);
+
+    Task<bool> UpdateProduct(Product product);
+    Task<bool> DeleteProduct(string id);
+    Task<bool> DeleteProduct(Product product);
 }
---
+++ Services/Catalog.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,52 @@
+﻿using Catalog.Core.Entities;
+using Catalog.Core.Repositories;
+
+namespace Catalog.Infrastructure.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    public Task<bool> DeleteProduct(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteProduct(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Product>> GetAllProducts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product> GetById(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Product>> GetProductsByBrand(string brand)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Product>> GetProductsByBrandId(string brandId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Product>> GetProductsByName(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Product>> GetProductsByTypeId(string typeId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateProduct(Product product)
+    {
+        throw new NotImplementedException();
+    }
+}
