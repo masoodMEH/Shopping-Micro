60d0530f887fe95218f162016705c13c4fcc3da0
masood <5045941-masoodMEH@users.noreply.gitlab.com>
2026-03-24 14:50:46 +0330

    adding mongodb driver and creating BaseEntity,Product Entities

---
+++ Services/Catalog.Core/Entities/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.Core.Entities;
+
+public class BaseEntity
+{
+    [BsonId] public string Id { get; set; }
+}
---
+++ Services/Catalog.Core/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.Core.Entities;
+
+public class Product : BaseEntity
+{
+    [BsonElement(nameof(Name))] public string Name { get; set; }
+    public string Summary { get; set; }
+    public string Description { get; set; }
+    public string ImageFile { get; set; }
+    public object Brands { get; set; }
+    public object Types { get; set; }
+
+    [BsonRepresentation(BsonType.Decimal128)] public decimal Price { get; set; }
+
+    // navigation property
+    public object Brands { get; set; }
+    public object Types { get; set; }
+
+}
